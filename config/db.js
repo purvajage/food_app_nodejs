4153eedaa0b8555bfdab60e670bff06384006e8e
purva jage <purvajage44@gmail.com>
2024-12-05 12:48:57 +0530

    SOME CHANGES

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+const connectDb=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("connected to database ${mongoose.connection.host}");
+    }
+    catch(error){
+        console.log("db error",error);
+
+    }
+};
+module.exports=connectDb;
---
+++ middleware/authmiddleware.js
@@ -0,0 +1,24 @@
+const JWT =require("jsonwebtoken");
+module.exports=async(req,res,next)=>{
+    try{
+        const token=req.headers["authorization"].split("")[1];
+        JWT.verify(token,process.env.JWT_SECRET,(err,decode)=>{
+            if(err){
+                return res.status(401).send({
+                    sucess:false,
+                    message:"un-authorize user",
+                });
+            }else{
+                req.body.id=decode.id;
+                next();
+            }
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            sucess:false,
+            message:"please provide auth token",
+            error,
+        });
+    }
+}
